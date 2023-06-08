c0e7b53b340cf47fdab507d556773d86df77d131
igorcossta <igorcossta21@gmail.com>
2023-06-08 11:31:38 -0300

    java maven project

---
+++ src/main/java/io/igorcossta/App.java
@@ -0,0 +1,13 @@
+package io.igorcossta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
